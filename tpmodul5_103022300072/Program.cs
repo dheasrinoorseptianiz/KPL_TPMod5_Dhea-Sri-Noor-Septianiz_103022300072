c22521e35804c92cb681705ba145832350395dd5
dheasrinoorseptianiz <dheasrinoorseptianiz02@gmail.com>
2025-03-15 21:39:13 +0700

    menambahkan class DataGeneric

--- tpmodul5_103022300072/Program.cs
+++ tpmodul5_103022300072/Program.cs
@@ -1,2 +1,38 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!!");
+
+using System;
+class HaloGeneric
+{
+    public void SapaUser<T>(T user)
+    {
+        Console.WriteLine($"Halo user {user}");
+    }
+}
+
+class DataGeneric<T>
+{
+    private T data;
+    public DataGeneric(T data)
+    {
+        this.data = data;
+    }
+
+    public void PrintData()
+    {
+        Console.WriteLine($"Data yang tersimpan adalah: {data}");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        HaloGeneric halo = new HaloGeneric();
+        halo.SapaUser("Dhea");
+
+        DataGeneric<string> dataNIM = new DataGeneric<string>("103022300072"); 
+        dataNIM.PrintData();
+    }
+}
+
+
